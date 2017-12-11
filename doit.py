fa01364996f7eb1feb3dc2ce60ba9baeef48356c
makefu <github@syntax-fehler.de>
2017-12-11 18:07:26 +0100

    first working version

--- doit.py
+++ doit.py
@@ -3,11 +3,13 @@
 Options:
     -I P            explicitly set NIX_PATH to P
     --try-build     try to build the new derivation
+    --force-rehash  force the rehashing even if the version number is lower
 
 If no VERSION is given, the script will try to find the latest release
 
 """
 from nix import eval
+import fileinput
 from docopt import docopt
 import logging as log
 from os import environ
@@ -19,6 +21,32 @@ import re
 import requests
 #log.basicConfig(level=log.INFO)
 log.basicConfig(level=log.DEBUG)
+
+def replaceLast(loc,end_line,old,new,matcher):
+    last_line = None
+
+    with open(loc,"r") as srcfile:
+        # python-packages:
+
+        for num,line in enumerate(srcfile):
+            if matcher.search(line):
+                last_line = num +1 # enumerate counts from 0
+                log.debug(f"Possible match at {num}: {line.strip()}")
+            if num == end_line:
+                log.debug("finished search")
+                break
+
+    if not last_line:
+        raise Exception(f"Unable to find the {matcher} line of {expr}")
+
+    for line in fileinput.input(loc,inplace=True):
+        if fileinput.filelineno() == last_line:
+            log.info("replacing {old} with {new} on line {num}")
+            log.debug(f"old line: {line.strip()}")
+            line = line.replace(str(old),str(new))
+            log.debug(f"new line: {line.strip()}")
+        sys.stdout.write(line)
+    return True
 
 def fetchUrl(url):
     from subprocess import check_output,DEVNULL
@@ -51,10 +79,10 @@ def pypiRelease(url):
 
 
 
-def guessNewVersion(name,current_version,urls):
+def guessNewVersion(name,current_version,urls,force_rehash=False):
     # TODO: try more than the first url
     url = urls[0]
-    
+
     possibleServices = [ githubTags, pypiRelease]
     for srv in possibleServices:
         try:
@@ -67,7 +95,7 @@ def guessNewVersion(name,current_version,urls):
         if versions:
             log.debug(f"found versions: {[str(version) for version in versions]}")
             new_version = max(versions)
-            if new_version > current_version:
+            if (new_version > current_version) or force_rehash:
                 return new_version
             else:
                 raise Exception(f"Current version {current_version} >= new {new_version}")
@@ -87,14 +115,16 @@ def main():
     nix_path = environ["NIX_PATH"] = expanduser(args["-I"] or environ["NIX_PATH"])
     new_version = args["VERSION"]
     new_hash = args["HASH"]
+    force_rehash = args['--force-rehash']
 
     log.info(f"This system: {system}")
     log.info(f"NIX_PATH: {nix_path}")
     try:
         nv = neval(f"builtins.parseDrvName {expr}.name")
         name,version = nv['name'],parse_version(nv['version'])
-        loc = neval(f"{expr}.meta.position").split(":")[0]
-        log.info(f"File for {name}-{version} is {loc}")
+        loc,locline = neval(f"{expr}.meta.position").split(":")
+        locline = int(locline)
+        log.info(f"File for {name}-{version} is {loc}:{locline}")
         hash_algo = neval(f"{expr}.src.drvAttrs.outputHashAlgo")
         hash = neval(f"{expr}.src.drvAttrs.outputHash")
         # TODO: resolve mirrors
@@ -114,12 +144,26 @@ def main():
     if not new_version:
         log.info(f"Trying to guess new version for {name}")
         try:
-            new_version = guessNewVersion(name,version,urls)
+            new_version = guessNewVersion(name,version,urls,force_rehash)
         except Exception as e:
             log.error(e)
             sys.exit(1)
         log.info(f"Found newer version for {name}: {new_version} (> {version})")
-    # fetchedHash = fetchUrl(urls[0])
+
+    matcher = re.compile(f'(version|rev)\s*=\s*"{version}";$')
+    replaceLast(loc,locline,version,new_version,matcher)
+    # TODO: if is git
+    if True:
+        urls = neval(f"{expr}.src.drvAttrs.urls")
+        new_hash = fetchUrl(urls[0])
+        log.info(f"new hash for {expr} is {new_hash}")
+        # TODO: not sure if hash_algo always matches
+        matcher = re.compile(f'({hash_algo})\s*=\s*"{hash}";$')
+        replaceLast(loc,locline,hash,new_hash,matcher)
+
+
+
+
 
 if __name__ == "__main__":
     main()
