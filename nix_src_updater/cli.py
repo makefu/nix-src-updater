092d645335ffd10cc76114d4407f8184ea1f9c2b
makefu <github@syntax-fehler.de>
2017-12-12 17:55:31 +0100

    mv to nix_src_updater

--- nix_src_updater/cli.py
+++ nix_src_updater/cli.py
@@ -238,7 +238,9 @@ def main():
     sys.exit(0)
 
 
-
-if __name__ == "__main__":
+def allowAllMain():
     with allowAllEvaluations():
         main()
+
+if __name__ == "__main__":
+    allowAllMain()
