24c9bce23abfc4640e39e1b62a29cda140d0a4bc
makefu <github@syntax-fehler.de>
2017-12-12 11:42:22 +0100

    add evaluation of broken, build packages at the end

--- doit.py
+++ doit.py
@@ -1,17 +1,26 @@
+#!/usr/bin/env python3.6
 """ usage: update-version [options] EXPR [VERSION [HASH]]
 
 Options:
     -I P            explicitly set NIX_PATH to P
     --try-build     try to build the new derivation
-    --force-rehash  force the rehashing even if the version number is lower
+    --lol LOL       set the log level [Default: INFO]
+    --force         force the rehashing even if the version number is lower
+    --no-build      do not try to build the expression at the end
 
 If no VERSION is given, the script will try to find the latest release
 
+Caveats:
+    Currently only works with packages which have a meta subsection in the same file as the source.
+    If the src rev or version is not derived from the derivation version the script will do nothing, however you can change the rev manually and rerun with --force
+
 """
+import json
 from nix import eval
 import fileinput
+import tempfile
 from docopt import docopt
-import logging as log
+import logging
 from os import environ
 from os.path import expanduser
 from pkg_resources import parse_version as parse_version
@@ -19,8 +28,26 @@ from pkg_resources import SetuptoolsVersion as Version
 import sys
 import re
 import requests
-#log.basicConfig(level=log.INFO)
-log.basicConfig(level=log.DEBUG)
+from subprocess import check_output,DEVNULL,CalledProcessError
+from contextlib import contextmanager
+
+log = logging.getLogger('cli')
+
+@contextmanager
+def allowAllEvaluations():
+    with tempfile.NamedTemporaryFile() as cfg:
+        cfg.write(b"pkgs: { allowUnfree = true; allowBroken = true; }")
+        cfg.flush()
+        environ['NIXPKGS_CONFIG'] = cfg.name
+        yield
+
+def setLOL(lol):
+  numeric_level = getattr(logging,lol.upper(),None)
+  if not isinstance(numeric_level,int):
+    raise AttributeError('No such log level {}'.format(lol))
+  logging.basicConfig(level=numeric_level)
+  log.setLevel(numeric_level)
+
 
 def replaceLast(loc,end_line,old,new,matcher):
     last_line = None
@@ -37,22 +64,45 @@ def replaceLast(loc,end_line,old,new,matcher):
                 break
 
     if not last_line:
-        raise Exception(f"Unable to find the {matcher} line of {expr}")
+        raise Exception(f"Unable to find the {matcher} line of {old} -> {new}")
 
     for line in fileinput.input(loc,inplace=True):
         if fileinput.filelineno() == last_line:
-            log.info("replacing {old} with {new} on line {num}")
+            log.info(f"replacing {old} with {new} on line {last_line}")
             log.debug(f"old line: {line.strip()}")
             line = line.replace(str(old),str(new))
             log.debug(f"new line: {line.strip()}")
         sys.stdout.write(line)
     return True
 
+def buildExpression(expr):
+    log.info(f"Building {expr}")
+    # TODO: we always want to see the build, right? If not:
+    ## out = None if (log.getEffectiveLevel() == logging.DEBUG) else DEVNULL
+    return check_output(f"nix-build -A {expr} <nixpkgs>".split(),
+            stderr=None).decode().strip()
+
+def fetchGit(attrs):
+    """ takes the dict of drvAttrs as input """
+
+    call = [ "nix-prefetch-git",attrs["url"],"--rev",attrs["rev"] ]
+    from pprint import pprint
+    if attrs["deepClone"]: call.append("--deepClone")
+    if attrs["leaveDotGit"]: call.append("--leave-dotGit")
+    if attrs["fetchSubmodules"]: call.append("--fetch-submodules")
+    log.info(f"Fetching git as {call}")
+
+    out = None if (log.getEffectiveLevel() == logging.DEBUG) else DEVNULL
+    return json.loads(check_output(call,
+        stderr=DEVNULL).decode())["sha256"]
+
 def fetchUrl(url):
-    from subprocess import check_output,DEVNULL
     log.info(f"Fetching {url}")
-    return check_output(["nix-prefetch-url", url],
-            stderr=DEVNULL).decode().strip()
+
+    out = None if (log.getEffectiveLevel() == logging.DEBUG) else DEVNULL
+    call = ["nix-prefetch-url", url]
+    log.debug(f"{call} - {out}")
+    return check_output(call,stderr=out).decode().strip()
 
 def githubTags(url):
     # github archive, TODO releases
@@ -98,10 +148,9 @@ def guessNewVersion(name,current_version,urls,force_rehash=False):
             if (new_version > current_version) or force_rehash:
                 return new_version
             else:
-                raise Exception(f"Current version {current_version} >= new {new_version}")
-                
+                raise ValueError(f"Current version {current_version} >= new {new_version}")
 
-    # 'mirror://pypi/r/ropper/ropper-1.10.10.tar.gz'
+    raise LookupError(f"Unable to find a valid version, please provide version manually")
 
 
 def neval(expr):
@@ -115,10 +164,14 @@ def main():
     nix_path = environ["NIX_PATH"] = expanduser(args["-I"] or environ["NIX_PATH"])
     new_version = args["VERSION"]
     new_hash = args["HASH"]
-    force_rehash = args['--force-rehash']
+    force = args['--force']
+    no_build = args['--no-build']
+
+    setLOL(args['--lol'])
 
     log.info(f"This system: {system}")
     log.info(f"NIX_PATH: {nix_path}")
+
     try:
         nv = neval(f"builtins.parseDrvName {expr}.name")
         name,version = nv['name'],parse_version(nv['version'])
@@ -127,10 +180,12 @@ def main():
         log.info(f"File for {name}-{version} is {loc}:{locline}")
         hash_algo = neval(f"{expr}.src.drvAttrs.outputHashAlgo")
         hash = neval(f"{expr}.src.drvAttrs.outputHash")
+
+        is_git = neval(f"builtins.hasAttr ''rev'' {expr}.src.drvAttrs")
         # TODO: resolve mirrors
-        try:
+        if not is_git:
             urls = neval(f"{expr}.src.drvAttrs.urls")
-        except:
+        else:
             log.debug("Using url instead of urls in drvAttrs")
             urls = [ neval(f"{expr}.src.drvAttrs.url") ]
         log.info(f"for urls {urls}")
@@ -144,26 +199,43 @@ def main():
     if not new_version:
         log.info(f"Trying to guess new version for {name}")
         try:
-            new_version = guessNewVersion(name,version,urls,force_rehash)
+            new_version = guessNewVersion(name,version,urls,force)
         except Exception as e:
             log.error(e)
             sys.exit(1)
         log.info(f"Found newer version for {name}: {new_version} (> {version})")
 
-    matcher = re.compile(f'(version|rev)\s*=\s*"{version}";$')
+    matcher = re.compile(f'(version|rev)\s*=\s*"{version}";\s*(#.*)?$')
     replaceLast(loc,locline,version,new_version,matcher)
     # TODO: if is git
-    if True:
-        urls = neval(f"{expr}.src.drvAttrs.urls")
-        new_hash = fetchUrl(urls[0])
-        log.info(f"new hash for {expr} is {new_hash}")
-        # TODO: not sure if hash_algo always matches
-        matcher = re.compile(f'({hash_algo})\s*=\s*"{hash}";$')
-        replaceLast(loc,locline,hash,new_hash,matcher)
+    if is_git:
+        attrs = neval(f"{expr}.src.drvAttrs")
+        url = attrs['url']
+        rev = attrs['rev']
+        new_hash = fetchGit(attrs)
+    else:
+        url = neval(f"{expr}.src.drvAttrs.urls")[0]
+        new_hash = fetchUrl(url)
 
+    log.info(f"new hash for {expr} is {new_hash}")
 
+    # TODO: not sure if hash_algo always matches
+    matcher = re.compile(f'({hash_algo})\s*=\s*"{hash}";$\s*(#.*)?')
+    replaceLast(loc,locline,hash,new_hash,matcher)
+    if not no_build:
+        log.info(f"trying to build changed expression {expr}")
+        try:
+            buildExpression(expr)
+        except CalledProcessError:
+            log.error(f"Unable to build {expr}, go ahead and follow the white rabbit")
+            sys.exit(1)
+    else:
+        log.info(f"Build of {expr} skipped")
+    log.info("Finished")
+    sys.exit(0)
 
 
 
 if __name__ == "__main__":
-    main()
+    with allowAllEvaluations():
+        main()
